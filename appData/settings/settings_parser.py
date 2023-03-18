baf820727f8de051415817d005219dd6bbc0e884
RomanRudin <rudin.roman@bk.ru>
2023-03-19 01:59:33 +0200

    Nope. Hotfix for parsing settings and that's all. I'm tired, I wanna sleep...

--- appData/settings/settings_parser.py
+++ appData/settings/settings_parser.py
@@ -1,7 +1,17 @@
-with open('config.txt', encoding='utf8') as file:
-    SCHEDULE = bool(file.readline()[file.readline().find(' = '):])
-    MANAGEMENT = bool(file.readline()[file.readline().find(' = '):])
-    HEALTH = bool(file.readline()[file.readline().find(' = '):])
+'''
+If you want to read False as False from file in Python, you gonna need to use this crunch, because only empty sting will return False if converted to bool
+'''
+def normal_reader(text):
+    if text.lower().capitalize() == 'False':
+        return False
+    return True
+
+
+with open('appData/settings/config.txt', encoding='utf-8') as file:
+    data = file.read().splitlines()
+    SCHEDULE = normal_reader(data[0][data[0].find(' = ') + 3:])
+    MANAGEMENT = normal_reader(data[1][data[1].find(' = '):])
+    HEALTH = normal_reader(data[2][data[2].find(' = '):])
 
 
 
@@ -10,7 +20,7 @@ if SCHEDULE:
 
 
 if MANAGEMENT:
-    with open(f'management/norm.txt', encoding='utf-8') as file:
+    with open(f'appData/settings/management/norm.txt', encoding='utf-8') as file:
         BARS = [type_[:type_.find(' = ')] for type_ in file.read().splitlines() if type_[type_.find(' = '):]]
 
 
